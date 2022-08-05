c6e9258a9aefce3e90fe7c3211af64b533d4d844
Aleksandr Savichev <aleksandrs.hd@yandex.ru>
2022-08-05 16:55:51 +0300

    5

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,15 +2,7 @@ import MyPostsCss from './MyPosts.module.css';
 import Post from "./Post/Post";
 
 
-const MyPosts = () => {
-  const posts = [
-    {id: 1, message: 'Have a nice day', likesCount: 29},
-    {id: 2, message: 'How are you?', likesCount: 16},
-    {id: 3, message: 'Have a nice weekend', likesCount: 41},
-    {id: 4, message: 'Weather is fine', likesCount: 55},
-    {id: 5, message: 'My second post', likesCount: 9},
-    {id: 6, message: 'My first post', likesCount: 99}
-  ];
+const MyPosts = (posts) => {
 
   const postsElements = posts.map(post => <Post message={post.message} likes={post.likesCount}/>);
   return (
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,10 +3,19 @@ import MyPosts from "./MyPosts/MyPosts";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 const Profile = () => {
+  const postsD = [
+    {id: 1, message: 'Have a nice day', likesCount: 29},
+    {id: 2, message: 'How are you?', likesCount: 16},
+    {id: 3, message: 'Have a nice weekend', likesCount: 41},
+    {id: 4, message: 'Weather is fine', likesCount: 55},
+    {id: 5, message: 'My second post', likesCount: 9},
+    {id: 6, message: 'My first post', likesCount: 99}
+  ];
+
   return (
     <main className={ProfileCss.profile}>
       <ProfileInfo/>
-      <MyPosts/>
+      <MyPosts posts={postsD}/>
     </main>
   )
 }
